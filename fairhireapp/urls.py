601be53515983a21702b62610bfd686493c07f99
Jeet <jeetparikh1009@gmail.com>
2023-09-08 22:52:41 -0400

    Project Created

---
+++ fairhireapp/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path,include
+
+from . import views
+from fairhireapp.backend import login,complaint, home, about, laws, registeruser, logout, new_complaint, change_complaint_status
+# from product_analysis.apidata import complaint, home,login, signup, customize, save, toedit, edit, show_files,data,show_product, show_all_product, delete, getCardData, formSave
+
+urlpatterns = [
+
+    path('',home),
+    path('login',login),
+    path('complaint',complaint),
+    path('new_complaint',new_complaint),
+    path('about',about),
+    path('laws',laws),
+    path('registeruser',registeruser),
+    path('logout', logout),
+    path('change_complaint_status',change_complaint_status)
+    # path('login',login),
+    # path('signup',signup),
+    # path('customize/<str:userid>',customize, name="customize"),
+    # path('home/<str:userid>',customize),
+    # path('editfile/<str:userid>/<str:fileid>/<str:platform>/<str:brand>/<str:type>',toedit, name="namedurl"),
+    # path('save/',save),
+    # path('sort/',data),
+    # path('edit/',edit),
+    # path('files/<str:userid>',show_files, name="showfiles"),
+    # path('product/<str:id>/<str:userid>',show_product),
+    # path('items/<str:userid>/<str:brand>/<str:bookmark>',show_all_product),
+    # path('delete/<str:fileid>', delete),
+    # path('getCardData/', getCardData),
+    # path('formSave', formSave)
+    
+
+
+]
+
